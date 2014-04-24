0f482998f24c5dec64ba2720785cdef64f39acf6
THN1Q <Killroan@gmail.com>
2014-04-25 00:55:27 +0300

    Solution applied.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,56 @@
-## Put comments here that give an overall description of what your
-## functions do
+## A set of functions which calculate and cache the inverse of a matrix for
+## later usage.
 
-## Write a short comment describing this function
+## This function creates a special "matrix" object that can cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
+        
+        i <- NULL 
+        
+        ## this object stores the inverse of the matrix
+        
+        set <- function(y) {
+                x <<- y
+                inverse <<- NULL
+        }
+        
+        ## stores the matrix in the global environment and resets inverse matrix
+        
+        get <- function () x
+        
+        ## prints the matrix
+        
+        setinverse <- function(inverse) i <<- inverse
+        
+        ## stores inverse matrix of x in the global environment
+        
+        getinverse <- function() i
+        
+        ## prints inverse matrix of x
+        
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 
+        ## function interface for makeCacheMatrix
 }
 
-
-## Write a short comment describing this function
+## This function computes the inverse of the special "matrix" returned by 
+## makeCacheMatrix. If the inverse has already been calculated, it retrieves
+## the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        
+        i <- x$getinverse()
+        if(!is.null(i)) {
+                message("getting cached data")
+                return(i)
+        }
+        
+        ## checks for cached inverse matrix of x and returns it if available       
+        
+        data <- x$get()       ## retrieves original matrix
+        i <- solve(data, ...) ## calculates inverse matrix of x
+        x$setinverse(i)       ## stores calculated inverse matrix in GE
+        i                     ## printes inverse matrix
 }
